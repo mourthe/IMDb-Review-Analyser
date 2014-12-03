4ca457216378444aab5863c60c080c8d830db776
Gabriel Arrais <gabriel.arrais@vtex.com.br>
2014-12-03 10:35:35 -0200

    adapta caminho do java, dataset no repo e resultado svm_java_linear

--- java/src/Main.java
+++ java/src/Main.java
@@ -7,8 +7,9 @@ public class Main {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		
-				Svm svm = new Svm("C:\\Projects\\Golang\\src\\dataset.arff", 10);
+		        //"C:\Projects\ai2014-ml\dataset.arff"
+				//Svm svm = new Svm("C:\\Projects\\Golang\\src\\dataset.arff", 10);
+                Svm svm = new Svm("C:\\Projects\\ai2014-ml\\dataset.arff", 10);
                 svm.buildModel();
 				System.out.println(svm.getResume());
 				System.out.println("Teste de classificac�o real: " + svm.doRealTest());
